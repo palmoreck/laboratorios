46c72692edb804f4f9cbc217af38dca483e06c7c
Erick Palacios Moreno <palmoreck@gmail.com>
2021-01-19 18:36:14 -0600

    changing name talleres for laboratorios

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b algebra-lineal-1 --single-branch https://github.com/palmoreck/talleres.git
+cd ${HOME} && git clone -b algebra-lineal-1 --single-branch https://github.com/palmoreck/laboratorios.git
 exec "$@"
