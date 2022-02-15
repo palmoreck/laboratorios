1deb8885f94d32ec66fa71ea28274235fb0068eb
Erick Palacios Moreno <palmoreck@gmail.com>
2022-02-14 18:47:05 -0600

    using different name branch

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 NB_USER=jovyan
 HOME=/home/${NB_USER}
-cd ${HOME} && git clone -b algebra_lineal_2_2021_1 --single-branch https://github.com/palmoreck/laboratorios.git
+cd ${HOME} && git clone -b algebra_lineal_2 --single-branch https://github.com/palmoreck/laboratorios.git
 exec "$@"
